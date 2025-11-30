32b366f07643e02d8ffbcbb96daf71005651910c
Nikolai Kolesnikov <zveroboy85@gmail.com>
2025-11-30 18:40:34 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://images.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
